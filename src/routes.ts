95033542f2d56673055eda641f69e402a3ca7363
CovDev <shusten@hotmail.com>
2021-04-22 16:05:32 -0300

    :construction: chore: Adiciona rota para testar

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,35 @@
-import { Router } from "express";
+import { request, response, Router } from "express";
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "./repositories/SettingsRepository";
 
 const routes = Router();
+
+/**
+ * Tipos de parametros
+ * Routes Params => Parametros de rotas
+ * http://logalhost:3333/settings/1
+ * 
+ * Query Params => Filtros e buscas
+ * http://localhost:3333/settings/1?search=algumacoisa
+ * 
+ * Body params => {
+ *      JSON
+ * }
+ */
+
+routes.post("/settings", async (request, response) => {
+    const { chat, username } = request.body;
+
+    const settingsRepository = getCustomRepository(SettingsRepository);
+
+    const settings = settingsRepository.create({
+        chat,
+        username
+    })
+
+    await settingsRepository.save(settings);
+
+    return response.json(settings);
+})
 
 export { routes };

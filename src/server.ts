b01a3703e991dec342293e046154ca1a44e5a282
CovDev <shusten@hotmail.com>
2021-04-22 15:13:46 -0300

    :fire: remove: Remove codigo desnecessario

--- src/server.ts
+++ src/server.ts
@@ -4,25 +4,5 @@ import "./database";
 
 const app = express();
 
-app.get("/", ( request, response ) =>  {
-    return response.json({
-        "title": "JSON Example",
-        "nested": {
-          "someInteger": 7,
-          "someBoolean": true,
-          "someArray": [
-            "list of",
-            "fake strings",
-            "and fake keys"
-          ]
-        }
-    })
-})
-
-app.post("/users", ( request, response ) =>  {
-    return response.json({
-    message: "Usuário salvo com sucesso!"
-    })
-})
 
 app.listen(3333, () => console.log("Server is running on port 3333"));

467437ac6a96832f87440f93c16d08833213af7e
CovDev <shusten@hotmail.com>
2021-04-22 16:39:23 -0300

    :construction: chore: Adiciona controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,5 @@
-import { request, response, Router } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "./repositories/SettingsRepository";
+import { Router } from "express";
+import { SettingsController } from "./database/controllers/SettingsController";
 
 const routes = Router();
 
@@ -17,19 +16,8 @@ const routes = Router();
  * }
  */
 
-routes.post("/settings", async (request, response) => {
-    const { chat, username } = request.body;
+const settingsController = new SettingsController();
 
-    const settingsRepository = getCustomRepository(SettingsRepository);
-
-    const settings = settingsRepository.create({
-        chat,
-        username
-    })
-
-    await settingsRepository.save(settings);
-
-    return response.json(settings);
-})
+routes.post("/settings", settingsController.create);
 
 export { routes };
